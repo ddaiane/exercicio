558cd2a48042e898af81e59921faeb0544b720e3
Daiane <daianemarcon@gmail.com>
2021-09-09 18:17:12 -0300

    calculaMedia

--- parser.js
+++ parser.js
@@ -1,33 +1,64 @@
-const fs = require('fs'); 
+const fs = require('fs');
 const parser = require('csv-parser');
 const process = require('process');
+/* 
+1. [Consultar média de idade dos pacientes] Permitir que o usuário informe o nome do
+município residencial e como resultado o programa deverá exibir:
+a. O número total de pacientes do município;
+b. A média de idade dos pacientes separados por gênero;
+c. A média de idade de todos os pacientes; */
 
-
-
-function manipulaCSV() { 
+function mediaIdade() {
     const filePath = __dirname + process.argv[2];
     console.log(filePath);
 
-    fs.createReadStream(filePath)
-.pipe(parser({ separator: ';' }))
-.on('data', function(data){
-    try {
-        
-        if(data["idade"] > 100 && data["sexo"] == "MASCULINO") {
-            console.log(data);
-        }
+    let municipioPesquisado = 'CANOAS';
+    let idadesPacientesMunicipio = []
+    let idadesF = [];
+    let idadesM = [];
 
-        //operacao em cada linha
-    }
-    catch(err) {
-        //tratamento de erros
-    }
-})
-.on('end',function(){
-    //ação no final do stream
-});  
+    fs.createReadStream(filePath)
+        .pipe(parser({
+            separator: ';'
+        }))
+        .on('data', function (data) {
+            try {
+                
+                if (data["municipio_residencia"] == municipioPesquisado) {
+
+                    let idades = parseInt(data["idade"]);
+
+                    if (data["sexo"] == 'MASCULINO') {
+                        idadesM.push(idades);
+                    } else if (data["sexo"] == 'FEMININO') {
+                        idadesF.push(idades);
+                    }
+                    
+                    idadesPacientesMunicipio.push(idades);
+                }
+
+            } catch (err) {
+                //tratamento de erros
+            }
+        })
+        .on('end', function () {
+            console.log("Numero total de pacientes da cidade de " + municipioPesquisado + " : " + idadesPacientesMunicipio.length);
+            console.log("Média de idade feminina: " + calculaMedia(idadesF));
+            console.log("Média de idade feminina: " + calculaMedia(idadesM));
+            console.log("Média de idade total: " + calculaMedia(idadesPacientesMunicipio));
+        });
 
 }
 
-manipulaCSV();
+mediaIdade();
 
+//utilitaria
+
+function calculaMedia(array) {
+    let somaIdades = 0;
+    for (i = 0; i < array.length; i++) {
+        somaIdades += array[i];
+    }
+    let saida = somaIdades / array.length;
+    return saida.toFixed(3);
+}
