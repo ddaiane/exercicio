ef2d6d40af57b279b9ca5b6335ca2b3789aa19d6
Daiane <daianemarcon@gmail.com>
2021-09-10 03:40:51 -0300

    começa implementar perguntas

--- funcoes/mediaIdade.js
+++ funcoes/mediaIdade.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const parser = require('csv-parser');
 const process = require('process');
+const inquirer = require('inquirer');
 /* 
 1. [Consultar média de idade dos pacientes] Permitir que o usuário informe o nome do
 município residencial e como resultado o programa deverá exibir:
@@ -8,12 +9,32 @@ a. O número total de pacientes do município;
 b. A média de idade dos pacientes separados por gênero;
 c. A média de idade de todos os pacientes; */
 
-function mediaIdade() {
+function inicio() {
+    //pede o municipio e chama funcao
+    const question = [
+        {
+          type: 'input',
+          name: 'municipio',
+          message: "Informe o município de residência desejado: ",
+        }
+    ]
+
+    inquirer.prompt(question).then((answer) => {
+        if(answer["municipio"] === "sair") {
+            var shell = require('shelljs');
+            shell.exec('Endlocal)');
+        }
+        const municipio = answer["municipio"].toUpperCase();
+        mediaIdade(municipio);
+      });
+}
+
+function mediaIdade(municipioPesquisado) {
     //const filePath = __dirname + process.argv[2];
     const filePath = __dirname + '\\dados.csv';
 
 
-    let municipioPesquisado = 'CANOAS';
+    //let municipioPesquisado = 'CANOAS';
     let idadesPacientesMunicipio = []
     let idadesF = [];
     let idadesM = [];
@@ -42,16 +63,23 @@ function mediaIdade() {
                 //tratamento de erros
             }
         })
+
         .on('end', function () {
-            console.log("\nNumero total de pacientes da cidade de " + municipioPesquisado + " : " + idadesPacientesMunicipio.length);
+            if(idadesPacientesMunicipio.length > 0) {
+                console.log("\nNumero total de pacientes da cidade de " + municipioPesquisado + " : " + idadesPacientesMunicipio.length);
             console.log("Média de idade feminina: " + calculaMedia(idadesF));
             console.log("Média de idade feminina: " + calculaMedia(idadesM));
             console.log("Média de idade total: " + calculaMedia(idadesPacientesMunicipio) + "\n");
+            }
+            else {
+                console.log("\nO município informado não consta nos dados");
+            }
+            
         });
 
 }
 
-mediaIdade();
+inicio();
 
 //utilitaria
 
@@ -65,4 +93,4 @@ function calculaMedia(array) {
 }
 
 
-module.exports = { mediaIdade };
+module.exports = { inicio };
